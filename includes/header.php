abdc62ec460111e6e7616e69a2202c79aafa7ed5
FlavioAraujo23 <flaviolopes1027@gmail.com>
2025-07-06 20:27:44 -0300

    adicionando o header

---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Desafio Revvo</title>
+  <link rel="stylesheet" href="assets/css/header.css">
+</head>
+
+<body>
+  <header>
+    <div class="wrapper d-flex">
+      <img src="../images/Logo_LEO.png" alt="Logo Leo" class="logo">
+      <nav class="d-flex">
+        <div class="input-wrapper">
+          <input type="search" id="search" placeholder="Pesquisar cursos">
+          <i class="icon fa fa-search fa-fw " aria-hidden="true"></i>
+        </div>
+        <span class="divider"></span>
+        <div class="user-wrapper d-flex">
+          <img class="avatar" src="https://img.freepik.com/vetores-premium/personagem-avatar-isolado_729149-194801.jpg"
+            alt="Imagem de perfil" width="40" height="40">
+          <div class="user-info d-flex">
+            <p class="text-nav">Seja bem vindo</p>
+            <p class="username">John Doe</p>
+          </div>
+          <i class="fa-solid fa-caret-down"></i>
+        </div>
+      </nav>
+    </div>
+  </header>
