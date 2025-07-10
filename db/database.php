32309eac34a206e977a17416b610672663d5edc5
FlavioAraujo23 <flaviolopes1027@gmail.com>
2025-07-09 22:35:54 -0300

    ajustando config do banco

--- db/database.php
+++ db/database.php
@@ -1,24 +1,14 @@
 <?php
-$config = parse_ini_file('config.env');
-// $dbHost = $config['DB_HOST'];
-// $dbPort = $config['DB_PORT'];
-// $dbName = $config['DB_NAME'];
-// $dbUser = $config['DB_USER'];
-// $dbPass = $config['DB_PASS'];
+$config = parse_ini_file('./config.env');
+$host = $config['host'];
+$port = $config['port'];
+$db = $config['db'];
+$user = $config['user'];
+$pass = $config['pass'];
 
-
-// $conn = new mysqli($dbHost, $dbUser, $dbPass, $dbName, $dbPort);
-
-$host = 'maglev.proxy.rlwy.net';
-$port = 32732;
-$user = 'root';
-$pass = 'aOajNextuAzYVIusqbEBkLjMgOkHipMV';
-$db = 'railway';
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
-// Verifica conexão
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
-
