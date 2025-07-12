d25e7389e6508f39a7c2d1d190f0db022bba6e49
FlavioAraujo23 <flaviolopes1027@gmail.com>
2025-07-11 21:45:27 -0300

    Refatorando: Adicionando a função update para atualização de dados no bd

--- includes/functions.php
+++ includes/functions.php
@@ -12,7 +12,7 @@ function create($type, $data = []): array
 
     $titulo = $data["titulo"];
     $descricao = $data["descricao"];
-    $link = $data["linkbtn"];
+    $link = $data["link"];
 
     //Tratamento da imagem
     if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
@@ -32,7 +32,7 @@ function create($type, $data = []): array
                 if ($stmt->execute()) {
                     return [
                         "status" => "success",
-                        "mensagem" => $type . "salvo com sucesso!",
+                        "mensagem" => $type . " salvo com sucesso!",
                     ];
                 } else {
                     return [
@@ -56,4 +56,72 @@ function create($type, $data = []): array
     ];
 }
 
+
+
+function update($type, $data = [], $id): array
+{
+    global $conn;
+
+    $query = $type === "Slide" ? "SELECT imagem FROM slides WHERE id = ?" : "SELECT imagem FROM cursos WHERE id = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $stmt->bind_result($imagemAntiga);
+    $stmt->fetch();
+    $stmt->close();
+
+    $titulo = $data["titulo"];
+    $descricao = $data["descricao"];
+    $link = $data["link"];
+
+
+    if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] !== UPLOAD_ERR_NO_FILE) {
+        if ($_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+            $nomeTemp = $_FILES['imagem']['tmp_name'];
+            $nomeFinal = "uploads/" . uniqid() . "-" . basename($_FILES['imagem']['name']);
+
+            if (move_uploaded_file($nomeTemp, $nomeFinal)) {
+                $novaImagem = $nomeFinal;
+
+                // Remove a imagem antiga
+                if ($imagemAntiga && file_exists($imagemAntiga)) {
+                    unlink($imagemAntiga);
+                }
+            } else {
+                return [
+                    "status" => "error",
+                    "mensagem" => "Erro ao salvar nova imagem.",
+                ];
+            }
+        } else {
+            return [
+                "status" => "error",
+                "mensagem" => "Erro ao fazer upload da imagem: código " . $_FILES['imagem']['error'],
+            ];
+        }
+    } else {
+        $novaImagem = $imagemAntiga;
+    }
+    try {
+        $query = $type === "Slide" ? "UPDATE slides SET titulo = ?, descricao = ?, link = ?, imagem = ? WHERE id = ?" : "UPDATE cursos SET titulo = ?, descricao = ?, link = ?, imagem = ? WHERE id = ?";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param('ssssi', $titulo, $descricao, $link, $novaImagem, $id);
+        if ($stmt->execute()) {
+            return [
+                "status" => "success",
+                "mensagem" => $type . " atualizado com sucesso!",
+            ];
+        } else {
+            return [
+                "status" => 'error',
+                "mensagem" => "Erro ao atualizar o" . $type . ":" . $stmt->error
+            ];
+        }
+    } catch (Exception $e) {
+        return [
+            "status" => 'error',
+            "mensagem" => "Erro ao atualizar o" . $type . ":" . $e->getMessage(),
+        ];
+    }
+}
 ?>
--- slides/atualizarSlide.php
+++ slides/atualizarSlide.php
@@ -1,85 +1,25 @@
 <?php
 
-require '../db/database.php';
+require "../includes/functions.php";
 
-if (!isset($_POST)) {
-    echo "Nenhum dado enviado";
-    exit;
-}
-
+$data = [
+    "titulo" => $_POST["titulo"],
+    "descricao" => $_POST["descricao"],
+    "link" => $_POST["linkbtn"]
+];
 $id = $_POST["id"];
-$imagem = $_POST["imagem"];
-$titulo = $_POST["titulo"];
-$descricao = $_POST["descricao"];
-$link = $_POST["linkbtn"];
 
-$stmt = $conn->prepare("SELECT imagem FROM slides WHERE id = ?");
-$stmt->bind_param("i", $id);
-$stmt->execute();
-$stmt->bind_result($imagemAntiga);
-$stmt->fetch();
-$stmt->close();
+$res = update('Slide', $data, $id);
 
-if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] !== UPLOAD_ERR_NO_FILE) {
-    if ($_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
-        $nomeTemp = $_FILES['imagem']['tmp_name'];
-        $nomeFinal = "uploads/" . uniqid() . "-" . basename($_FILES['imagem']['name']);
 
-        if (move_uploaded_file($nomeTemp, $nomeFinal)) {
-            $novaImagem = $nomeFinal;
+$toast = [
+    'text' => $res['mensagem'],
+    'type' => $res['status'],
+    'duration' => 4000
+];
+setcookie('toast', json_encode($toast), time() + 10, '/');
 
-            // Remove a imagem antiga
-            if ($imagemAntiga && file_exists($imagemAntiga)) {
-                unlink($imagemAntiga);
-            }
-        } else {
-            $toast = [
-                'text' => "Erro ao salvar nova imagem.",
-                'type' => 'error',
-                'duration' => 4000
-            ];
-            setcookie('toast', json_encode($toast), time() + 10, '/');
-
-            header('Location: /');
-            exit;
-        }
-    } else {
-        $toast = [
-            'text' => "Erro ao fazer upload da imagem: código " . $_FILES['imagem']['error'],
-            'type' => 'error',
-            'duration' => 4000
-        ];
-        setcookie('toast', json_encode($toast), time() + 10, '/');
-
-        header('Location: /');
-        exit;
-    }
-} else {
-    $novaImagem = $imagemAntiga;
-}
-
-$stmt = $conn->prepare('UPDATE slides SET titulo = ?, descricao = ?, link = ?, imagem = ? WHERE id = ?');
-$stmt->bind_param('ssssi', $titulo, $descricao, $link, $novaImagem, $id);
-if ($stmt->execute()) {
-    $toast = [
-        'text' => 'Slide atualizado com sucesso!',
-        'type' => 'success',
-        'duration' => 4000
-    ];
-    setcookie('toast', json_encode($toast), time() + 10, '/');
-
-    header('Location: /');
-    exit;
-} else {
-    $toast = [
-        'text' => "Erro ao atualizar Slide" . $stmt->error,
-        'type' => 'error',
-        'duration' => 4000
-    ];
-    setcookie('toast', json_encode($toast), time() + 10, '/');
-
-    header('Location: /');
-    exit;
-}
+header('Location: /');
+exit;
 
 ?>
